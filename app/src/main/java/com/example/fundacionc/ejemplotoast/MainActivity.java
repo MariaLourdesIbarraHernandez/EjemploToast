cfc65ed546e028669f9ed6cb46b1a10c344df86f
maria <maria_abasolo@hotmail.com>
2018-04-20 12:58:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fundacionc/ejemplotoast/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fundacionc.ejemplotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton boton ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton = (ImageButton) findViewById(R.id.imageButton);
+
+       boton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               LayoutInflater inflater = getLayoutInflater();
+
+               View layout = inflater.inflate(R.layout.toast, null);
+
+               Toast toast = new Toast(getApplicationContext());
+
+               toast.setDuration(Toast.LENGTH_LONG);
+
+               toast.setView(layout);
+
+               toast.show();
+           }
+       });
+    }
+}
